41e6d6a9bde2aebbc3322d2d7710d5286cec0ade
Alek Pialucha <pialuchaa@btinternet.com>
2023-09-04 20:34:13 +0100

    issue with updating outcome

--- src/App.js
+++ src/App.js
@@ -16,7 +16,6 @@ function App() {
       <div className="App">
         <PlayerChoice />
       </div>
-     <div>{singlePlayerChoice}</div>
      <ComputerChoice />
     </SinglePlayerChoiceContext.Provider>
   );
--- src/ComputerChoice.jsx
+++ src/ComputerChoice.jsx
@@ -1,24 +1,47 @@
-import { useContext, useEffect, useState } from "react"
-import SinglePlayerChoiceContext from "./SinglePlayerChoiceContext"
+import { useContext, useEffect, useState } from "react";
+import SinglePlayerChoiceContext from "./SinglePlayerChoiceContext";
 
 const ComputerChoice = () => {
-  const userChoice = useContext(SinglePlayerChoiceContext)
-  const computerOptions = ["rock", "paper", "scissors"]
+  const userChoice = useContext(SinglePlayerChoiceContext);
+  const computerOptions = ["rock", "paper", "scissors"];
 
-  const [computerChoice, setComputerChoice] = useState("none")
+  const [computerChoice, setComputerChoice] = useState("none");
+  const [outcome, setOutcome] = useState("hello");
 
   useEffect(() => {
-    setComputerChoice(computerOptions[Math.floor(Math.random() * computerOptions.length)])
-  }, [userChoice])
+    setComputerChoice(
+      computerOptions[Math.floor(Math.random() * computerOptions.length)],
+    );
 
+    // Determine the outcome based on userChoice and computerChoice
+    switch (userChoice) {
+      case "rock":
+        if (computerChoice === "rock") setOutcome("tie");
+        else if (computerChoice === "paper") setOutcome("lose");
+        else if (computerChoice === "scissors") setOutcome("win");
+        break;
+      case "paper":
+        if (computerChoice === "rock") setOutcome("win");
+        else if (computerChoice === "paper") setOutcome("tie");
+        else if (computerChoice === "scissors") setOutcome("lose");
+        break;
+      case "scissors":
+        if (computerChoice === "rock") setOutcome("lose");
+        else if (computerChoice === "paper") setOutcome("win");
+        else if (computerChoice === "scissors") setOutcome("tie");
+        break;
+      default:
+        break;
+    }
+  }, [userChoice, computerChoice]);
 
   return (
     <div>
       <div>{userChoice}</div>
-      <div>{computerOptions}</div>
       <div>{computerChoice}</div>
+      <div>{outcome}</div>
     </div>
-  )
-}
+  );
+};
 
-export default ComputerChoice
+export default ComputerChoice;
